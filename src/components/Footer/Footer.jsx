d4ffbb24ef7c073fbb30d5c6a9a3f43b88c09ec1
ace-niraj <aceop606@gmail.com>
2021-04-24 23:02:23 +0545

    'he"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,58 @@
 import React from "react";
 import useStyles from "./style";
+import logo from "../../assets/logo-1.png";
+import { useTheme } from "@material-ui/styles";
+import { Grid, useMediaQuery } from "@material-ui/core";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import TelegramIcon from "@material-ui/icons/Telegram";
+
 const Footer = () => {
+  const theme = useTheme();
   const classes = useStyles();
-  return <>footer</>;
+  const match = useMediaQuery(theme.breakpoints.down("xs"));
+  return (
+    <>
+      <footer className={classes.footer}>
+        <Grid
+          container
+          direction={match ? "column" : "row"}
+          justify={match ? "center" : "space-between"}
+          alignItems='center'
+        >
+          <Grid className={match ? `${classes.gridItem}` : ""} item>
+            <img className={classes.logo} src={logo} alt='footer logo' />
+          </Grid>
+          <Grid
+            className={
+              match
+                ? `${classes.gridItem} ${classes.copyright}`
+                : `${classes.copyright}`
+            }
+            item
+          >
+            Copyright &copy; 2021
+          </Grid>
+          <Grid
+            className={match ? `${classes.gridItem}` : `${classes.socialIcons}`}
+            item
+          >
+            <Grid container spacing={3}>
+              <Grid className={classes.socialIcon} item>
+                <FacebookIcon />
+              </Grid>
+              <Grid className={classes.socialIcon} item>
+                <LinkedInIcon />
+              </Grid>
+              <Grid className={classes.socialIcon} item>
+                <TelegramIcon />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,5 +1,31 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.common.black,
+    width: "100%",
+
+    color: theme.palette.common.white,
+  },
+  logo: {
+    height: "4rem",
+    marginLeft: "25px",
+  },
+  copyright: {
+    fontFamily: "Open Sans",
+  },
+  gridItem: {
+    margin: "15px auto",
+  },
+  socialIcons: {
+    marginRight: "25px",
+  },
+  socialIcon: {
+    "&:hover": {
+      color: theme.palette.common.blue,
+      cursor: "pointer",
+    },
+  },
+}));
 
 export default useStyles;
